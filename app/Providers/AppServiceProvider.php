d7e70e82b011eb4aaf39584ec5b8fed9891c705b
Alger Makiputin <algerzxc@gmail.com>
2018-12-19 20:00:39 +0800

    config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(config('app.env') === "production") {
+    
+        if(App::environment()) {
             \URL::forceScheme('https');
         }
     }
