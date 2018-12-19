adf071bf9fae261ac4b66744f786c3216d88c8d9
Alger Makiputin <algerzxc@gmail.com>
2018-12-19 09:24:04 +0800

    config

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', getenv("APP_URL")),
 
     /*
     |--------------------------------------------------------------------------
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => getenv("APP_KEY"),
 
     'cipher' => 'AES-256-CBC',
 
